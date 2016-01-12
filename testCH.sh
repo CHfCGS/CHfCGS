d3f4cb35037b7d780ced751d15e7ab3d56589e4b
CHfCGS <Alligator52@gmail.com>
2016-01-12 20:26:43 +0100

    test.sh

--- testCH.sh
+++ testCH.sh
@@ -1,4 +1,4 @@
-data=stuttgart-regbez #unbreakedDPVEPunfiltered #ChainPair2 # #bremen #ProDPExample #Intersection #baden-wuerttemberg #stuttgart-regbez #ZipOrderingTest #ProP_ILPChainPair #PairsToIdentify #IntersectionDiff #heapTest
+data=bremen #unbreakedDPVEPunfiltered #ChainPair2 # #bremen #ProDPExample #Intersection #baden-wuerttemberg #stuttgart-regbez #ZipOrderingTest #ProP_ILPChainPair #PairsToIdentify #IntersectionDiff #heapTest
 echo "data: " "$data"
 
 in=$data"-latest.osm.pbf"
@@ -8,11 +8,15 @@ out=$data"FMAXSPEED.txt"
 #OsmGraphCreator/build/creator/creator -g fmimaxspeedtext -t time -c OsmGraphCreator/data/configs/car.cfg -o data/Graph/$out data/OSM/$in
 
 #chconstructor
+
 options1=" -p DP -d EDE "
-options2=" -s DP -e VE "
+#options1="  "
+
+_options2=" -s BU -e KE "
+options2=${1-$_options2}
+
+#options2=" -p NONE " 
 echo "options2: " "$options2"
-
-
 
 out_options2=`echo "$options2" | sed "s/ /_/g"`
 in=$out
@@ -22,7 +26,7 @@ LOGFILE="$out$NOW.log"
 
 #chconstructor/build/ch_constructor -i data/Graph/$in -o data/CH/$out  -p DP -s DP -e VE -w P -d EDE -f FMI_DIST -g FMI_CH 2>&1 | tee chconstructor/log/$LOGFILE # -d EDE
 
-#chconstructor/build/ch_constructor -i data/Graph/$in -o data/CH/$out  $options1 $options2 -f FMI_DIST -g FMI_CH 2>&1 | tee chconstructor/log/$LOGFILE # -d EDE
+chconstructor/build/ch_constructor -i data/Graph/$in -o data/CH/$out  $options1 $options2 -f FMI_DIST -g FMI_CH 2>&1 | tee chconstructor/log/$LOGFILE # -d EDE
 
 
 #measurer
@@ -32,7 +36,7 @@ NOW=$(date +"%d_%H")
 LOGFILE="$in$NOW.log"
 
 
-./ch_measurer -i ../../data/CH/$in -p  2>&1 | tee ../logs/$LOGFILE    #-e -c -d -l -p -v 
+./ch_measurer -i ../../data/CH/$in -l 2>&1 | tee ../logs/$LOGFILE    #-e -c -d -l -p -v 
 
 
 #visualizer
