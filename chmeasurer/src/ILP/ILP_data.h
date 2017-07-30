bafa77b21822b9766d451e84d0ea4ed94273935d
CHfCGS <Alligator52@gmail.com>
2017-07-30 17:49:57 +0200

    Pull up fileds and methods

--- chmeasurer/src/ILP/ILP_data.h
+++ chmeasurer/src/ILP/ILP_data.h
@@ -7,8 +7,6 @@
 
 struct ILP_data : Data
 {
-    ILP_Chain ilp_chain1;
-    ILP_Chain ilp_chain2;
     LineID nextLineID = 0;
     std::vector<Line> potEdges1;
     std::vector<Line> potEdges2;
@@ -20,21 +18,6 @@ struct ILP_data : Data
     std::vector<Intersection> followerLinesUnorderings;
 
     //bool followerIsPartial = true; //if this is false, the structure is invalid
-
-    static bool testIntersection(const Line line1, const Line line2)
-    {
-
-        //4 Orientation tests
-        double line2StartArea = geo::calcSignedArea(line1.start.ch_node, line1.end.ch_node, line2.start.ch_node);
-        double line2EndArea = geo::calcSignedArea(line1.start.ch_node, line1.end.ch_node, line2.end.ch_node);
-        bool line1BetweenLine2 = geo::differentSign(line2StartArea, line2EndArea);
-
-        double line1StartArea = geo::calcSignedArea(line2.start.ch_node, line2.end.ch_node, line1.start.ch_node);
-        double line1EndArea = geo::calcSignedArea(line2.start.ch_node, line2.end.ch_node, line1.end.ch_node);
-        bool line2BetweenLine1 = geo::differentSign(line1StartArea, line1EndArea);
-
-        return line1BetweenLine2 && line2BetweenLine1;
-    }
 
     static std::vector<Intersection> calculateIntersections(const std::vector<Line> &lines)
     {
@@ -113,25 +96,6 @@ struct ILP_data : Data
         return followerLinesUnorderings;
     }
 
-    static double computeLineError(const Line &line, const ILP_Chain &ilp_chain, ILP_Chain::const_iterator srcIt, ILP_Chain::const_iterator tgtIt)
-    {
-        assert(srcIt != tgtIt);
-        assert(line.start.posInChain < line.end.posInChain);
-        assert(ilp_chain.size() >= 2);
-        double maxError = 0;
-
-        //calculate error for all nodes between src and tgt
-        for (auto it = ++srcIt; it != tgtIt; it++)
-        {
-            double error = geo::calcPerpendicularLength(line.start.ch_node, line.end.ch_node, it->ch_node);
-            if (error > maxError)
-            {
-                maxError = error;
-            }
-        }
-        return maxError;
-    }
-
     std::vector<Line> createPotentialEdges(const ILP_Chain &ilp_chain, double epsilon)
     {
         std::vector<Line> lines;
@@ -175,47 +139,6 @@ struct ILP_data : Data
             //followerIsPartial = !isFollowing;
         }
         return followerLines;
-    }
-
-    ILP_Chain ChainToILP_Chain(const Chain &chain, bool sameDirection) const
-    {
-        ILP_Chain ilp_chain;
-        uint posInChain = 0;
-        if (sameDirection)
-        {
-            for (Chain::const_iterator it = chain.begin(); it != chain.end(); it++)
-            {
-                NodeID ch_node_id = *it;
-                ILP_Node ilp_node = ILP_Node(graph.getNode(ch_node_id), ch_node_id, posInChain, true);
-                ilp_chain.push_back(ilp_node);
-                posInChain++;
-            }
-        } else
-        {
-            for (Chain::const_reverse_iterator it = chain.rbegin(); it != chain.rend(); it++)
-            {
-                NodeID ch_node_id = *it;
-                ILP_Node ilp_node = ILP_Node(graph.getNode(ch_node_id), ch_node_id, posInChain, false);
-                ilp_chain.push_back(ilp_node);
-                posInChain++;
-            }
-        }
-        return ilp_chain;
-
-    }
-
-    static std::vector<Line> concatLines(const std::vector<Line> &lines1, const std::vector<Line> &lines2)
-    {
-        std::vector<Line> concatenatedLines;
-        for (Line line : lines1)
-        {
-            concatenatedLines.push_back(line);
-        }
-        for (Line line : lines2)
-        {
-            concatenatedLines.push_back(line);
-        }
-        return concatenatedLines;
     }
 
     ILP_data(const CHGraph<CHNode, CHEdge> &graph, const Chain &chain1, const Chain &chain2, double epsilon, double eta, bool parallel)
--- chmeasurer/src/ILP/data.h
+++ chmeasurer/src/ILP/data.h
@@ -13,6 +13,82 @@ struct Data
     const CHGraph<CHNode, CHEdge> &graph;
     const Chain &chain1;
     const Chain &chain2;
+    ILP_Chain ilp_chain1;
+    ILP_Chain ilp_chain2;
+
+    static bool testIntersection(const Line line1, const Line line2)
+    {
+
+        //4 Orientation tests
+        double line2StartArea = geo::calcSignedArea(line1.start.ch_node, line1.end.ch_node, line2.start.ch_node);
+        double line2EndArea = geo::calcSignedArea(line1.start.ch_node, line1.end.ch_node, line2.end.ch_node);
+        bool line1BetweenLine2 = geo::differentSign(line2StartArea, line2EndArea);
+
+        double line1StartArea = geo::calcSignedArea(line2.start.ch_node, line2.end.ch_node, line1.start.ch_node);
+        double line1EndArea = geo::calcSignedArea(line2.start.ch_node, line2.end.ch_node, line1.end.ch_node);
+        bool line2BetweenLine1 = geo::differentSign(line1StartArea, line1EndArea);
+
+        return line1BetweenLine2 && line2BetweenLine1;
+    }
+
+    static double computeLineError(const Line &line, const ILP_Chain &ilp_chain, ILP_Chain::const_iterator srcIt, ILP_Chain::const_iterator tgtIt)
+    {
+        assert(srcIt != tgtIt);
+        assert(line.start.posInChain < line.end.posInChain);
+        assert(ilp_chain.size() >= 2);
+        double maxError = 0;
+
+        //calculate error for all nodes between src and tgt
+        for (auto it = ++srcIt; it != tgtIt; it++)
+        {
+            double error = geo::calcPerpendicularLength(line.start.ch_node, line.end.ch_node, it->ch_node);
+            if (error > maxError)
+            {
+                maxError = error;
+            }
+        }
+        return maxError;
+    }
+
+    ILP_Chain ChainToILP_Chain(const Chain &chain, bool sameDirection) const
+    {
+        ILP_Chain ilp_chain;
+        uint posInChain = 0;
+        if (sameDirection)
+        {
+            for (Chain::const_iterator it = chain.begin(); it != chain.end(); it++)
+            {
+                NodeID ch_node_id = *it;
+                ILP_Node ilp_node = ILP_Node(graph.getNode(ch_node_id), ch_node_id, posInChain, true);
+                ilp_chain.push_back(ilp_node);
+                posInChain++;
+            }
+        } else
+        {
+            for (Chain::const_reverse_iterator it = chain.rbegin(); it != chain.rend(); it++)
+            {
+                NodeID ch_node_id = *it;
+                ILP_Node ilp_node = ILP_Node(graph.getNode(ch_node_id), ch_node_id, posInChain, false);
+                ilp_chain.push_back(ilp_node);
+                posInChain++;
+            }
+        }
+        return ilp_chain;
+    }
+
+    static std::vector<Line> concatLines(const std::vector<Line> &line1, const std::vector<Line> &line2)
+    {
+        std::vector<Line> concatenatedLines;
+        for (Line line : line1)
+        {
+            concatenatedLines.push_back(line);
+        }
+        for (Line line : line2)
+        {
+            concatenatedLines.push_back(line);
+        }
+        return concatenatedLines;
+    }
 
     Data(const CHGraph<CHNode, CHEdge> &graph, const Chain &chain1, const Chain &chain2)
     : graph(graph), chain1(chain1), chain2(chain2) { }
